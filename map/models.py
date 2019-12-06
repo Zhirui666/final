eb0fdd5cdf1dcce3571af5e2e2729ada9a0f0e06
ll3351 <56945011+ll3351@users.noreply.github.com>
2019-12-05 21:08:27 -0500

    Update models.py

--- map/models.py
+++ map/models.py
@@ -1,3 +1,49 @@
+# map/model.py
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class sighting(models.Model):
+    Latitude = models.DecimalField(
+            max_digits = 100,
+            decimal_places = 10,
+            help_text = _("Latitude"),
+            )
+    Longitude = models.DecimalField(
+            max_digits = 100,
+            decimal_places = 10,
+            help_text = _("Longitude"),
+            )
+    Unique_Squirrel_ID = models.DecimalField(
+            max_digits = 100,
+            help_text = _("Squirrel ID"),
+            )
+    
+    PM = 'PM'
+    AM = 'AM'
+    
+    SHIFT_CHOICES = (
+        (PM, 'PM'),
+        (AM, 'AM'),
+        )
+    
+    Shift = models.CharField(
+        max_length = 100,
+        choices = SHIFT_CHOICES,
+        )
+    
+    Date = models.DateField(
+        help_text = _('Date'),
+        )
+    
+    
+    Adult = 'Adult'
+    Juvenile = 'Juvenile'
+    Other = 'Other'
+    
+    AGE_CHOICES = (
+        (),
+        (),
+        (),
+        )
+    
+    
