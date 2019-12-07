9e8856c1e2ebde30c41e5d02cf04c25767bd9b89
ll3351 <56945011+ll3351@users.noreply.github.com>
2019-12-07 18:38:05 -0500

    Update forms.py

--- sightings/forms.py
+++ sightings/forms.py
@@ -2,5 +2,5 @@ from django.forms import ModelForm
 from .models import sightings
 class SquForm(ModelForm):
     class Meta:
-        model = sightings
+        model = sightings_model
         fields = '__all__'
