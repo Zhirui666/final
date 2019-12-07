74b6ad8e772555c434bef50e9b2aee5acda4c2e8
Zhirui666 <56945028+Zhirui666@users.noreply.github.com>
2019-12-06 20:29:05 -0500

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name='sightings'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('stats/', views.stats, name='stats'),
+    path('<Unique_Squirrel_ID>/', views.edit, name = "edit"),
 ]
+
+
